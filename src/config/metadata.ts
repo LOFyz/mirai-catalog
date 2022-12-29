679a2e8f23ef15e0d5a76766b6ca91372980bdb3
LOFyz <leonardo_farias2010@hotmail.com>
2022-12-28 23:13:26 -0300

    feat: add mirai instagram

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -2,4 +2,5 @@ export default {
   title: `Use Mirai`,
   siteUrl: `https://www.usemirai.com.br`,
   description: `Elegância, Sol & Mar`,
+  social: [{ name: `instagram`, url: `https://www.instagram.com/use_mirai/` }],
 };
